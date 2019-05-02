4b0a3ccfb39785a9570aa27aec3d9de6d556b193
Abin Shaju T <abinshaju30@gmail.com>
2019-05-02 15:54:47 -0400

    Update globaltrade_scraper.py

--- globaltradescraper/globaltradescraper/spiders/globaltrade_scraper.py
+++ globaltradescraper/globaltradescraper/spiders/globaltrade_scraper.py
@@ -4,15 +4,28 @@ class GlobalTradeSpider(scrapy.Spider):
 	name="globaltradespider"
 	
         start_urls = [
-		'https://www.globaltrade.net/international-trade-import-exports/expert-service-provider-p/Fastfix-Inc.html'
+		'https://www.globaltrade.net/expert-service-provider.html'
 	]
 		
 
 	def parse(self, response):
 		
+		nextLink=response.css('.sp_country_71::attr(href)').get()
+		nexturl=response.urljoin(nextLink)
+		
+		yield scrapy.Request(url=nexturl, callback=self.innerPages)
+
+	def innerPages(self, response):
+		links=response.css('.profileNavigator::attr(href)').getall()
+		for link in links:
+			targeturl=response.urljoin(link)
+			yield scrapy.Request(url=targeturl, callback=self.targetPage)
+
+	def targetPage(self, response):
 		items=GlobaltradescraperItem()
-		items['title']=response.css('.sp-title span::text').extract()
-		items['sub_title']=response.css('.sub::text').extract()
-		items['primary_location']=response.css('.profile-details span span::text').extract()
+		items['title']=response.css('.sp-title span::text').get()
+		items['sub_title']=response.css('.sub::text').get()
+		items['primary_location']=response.css('.profile-details span span::text').get()
 		
 		yield items
+		
