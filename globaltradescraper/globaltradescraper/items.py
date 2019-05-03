7fb18d410c89ab9e2e513ad9278aec790784084c
Abin Shaju T <abinshaju30@gmail.com>
2019-05-03 18:14:33 -0400

    Update items.py

--- globaltradescraper/globaltradescraper/items.py
+++ globaltradescraper/globaltradescraper/items.py
@@ -6,12 +6,23 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import Join, MapCompose, TakeFirst
+from w3lib.html import remove_tags
+
 
 
 class GlobaltradescraperItem(scrapy.Item):
     # define the fields for your item here like:
+    logo_url=scrapy.Field()
     title = scrapy.Field()
     sub_title= scrapy.Field()
-   
     primary_location= scrapy.Field()
+    area_of_expertise=scrapy.Field()
+    about=scrapy.Field()
+    website=scrapy.Field()
+    language_spoken=scrapy.Field()
+    page_url=scrapy.Field()
+
+
+
     
